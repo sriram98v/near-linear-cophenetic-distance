01ac6b4de5e3280c5a0bb8ee0f287402384d3e80
sriram98v <sriram.98v@gmail.com>
2024-06-27 17:21:04 -0500

    created structs that implement node and tree attributes

--- src/nlcd.rs
+++ src/nlcd.rs
@@ -1,32 +1,109 @@
 pub mod near_linear_cophenetic_distance;
 
-use fxhash::FxHashSet as HashSet;
+use std::ops::{Index, IndexMut};
 
-use near_linear_cophenetic_distance::NearLinearCopheneticDistance;
+use near_linear_cophenetic_distance::{NearLinearCopheneticDistance, NlcdNodeAttributes, NlcdTreeAttributes, NlcdAttributeType};
+use phylo::prelude::*;
 use phylo::tree::SimpleRootedTree;
 
-// pub enum TaxaIntersectionType{
-//     UpperUpper,
-//     UpperLower,
-//     LowerUpper,
-//     LowerLower,
-// }
+#[derive(Debug, Clone)]
+pub struct NodeAttributes{
+    sigma: Vec<f32>,
+    sigma_pos: Vec<f32>,
+    sigma_neg: Vec<f32>,
+    delta: Vec<f32>,
+}
 
-// pub struct TaxaSets{
-//     a: HashSet<<SimpleRootedTree as NearLinearCopheneticDistance>::Meta>, 
-//     b: HashSet<<SimpleRootedTree as NearLinearCopheneticDistance>::Meta>,
-//     a_hat: HashSet<<SimpleRootedTree as NearLinearCopheneticDistance>::Meta>, 
-//     b_hat: HashSet<<SimpleRootedTree as NearLinearCopheneticDistance>::Meta>,
-// }
+impl NodeAttributes{
+    fn new(norm: u32)->NodeAttributes
+    {
+        NodeAttributes{
+            sigma: vec![0_f32; (norm as usize)+1],
+            sigma_pos: vec![0_f32; (norm as usize)+1],
+            sigma_neg: vec![0_f32; (norm as usize)+1],
+            delta: vec![0_f32; (norm as usize)+1],
+        }
+    }
+}
 
-// pub struct TaxaIntersections{
-//     a_int_a_hat: Vec<<SimpleRootedTree as NearLinearCopheneticDistance>::Meta>,
-//     a_int_b_hat: Vec<<SimpleRootedTree as NearLinearCopheneticDistance>::Meta>,
-//     b_int_a_hat: Vec<<SimpleRootedTree as NearLinearCopheneticDistance>::Meta>,
-//     b_int_b_hat: Vec<<SimpleRootedTree as NearLinearCopheneticDistance>::Meta>,
-// }
+impl Index<NlcdAttributeType> for NodeAttributes{
+    type Output = Vec<f32>;
+
+    fn index(&self, index: NlcdAttributeType) -> &Self::Output {
+        match index{
+            NlcdAttributeType::Sigma => {&self.sigma}
+            NlcdAttributeType::SigmaPos => {&self.sigma_pos}
+            NlcdAttributeType::SigmaNeg => {&self.sigma_neg}
+            NlcdAttributeType::Delta => {&self.delta}
+        }
+    }
+}
+
+impl IndexMut<NlcdAttributeType> for NodeAttributes{
+    fn index_mut(&mut self, index: NlcdAttributeType) -> &mut Self::Output {
+        match index{
+            NlcdAttributeType::Sigma => {&mut self.sigma}
+            NlcdAttributeType::SigmaPos => {&mut self.sigma_pos}
+            NlcdAttributeType::SigmaNeg => {&mut self.sigma_neg}
+            NlcdAttributeType::Delta => {&mut self.delta}
+        }
+    }
+}
+
+impl NlcdNodeAttributes<f32> for NodeAttributes{
+    fn reset(&mut self) {
+        for i in 0..self.sigma.len(){
+            self.sigma[i] = 0_f32;
+        }
+        for i in 0..self.sigma_pos.len(){
+            self.sigma_pos[i] = 0_f32;
+        }
+        for i in 0..self.sigma_neg.len(){
+            self.sigma_neg[i] = 0_f32;
+        }
+        for i in 0..self.delta.len(){
+            self.delta[i] = 0_f32;
+        }
+    }
+}
+
+pub struct TreeNodeAttributes{
+    attr: Vec<NodeAttributes>,
+}
+
+impl TreeNodeAttributes{
+    fn new(tree: &SimpleRootedTree, norm: u32)-> TreeNodeAttributes
+    {
+        let max_id = tree.get_node_ids().max().unwrap();
+        TreeNodeAttributes{
+            attr: vec![NodeAttributes::new(norm);max_id+1]
+        }
+    }
+}
+
+impl Index<<SimpleRootedTree as RootedTree>::NodeID> for TreeNodeAttributes{
+    type Output = NodeAttributes;
+
+    fn index(&self, index: <SimpleRootedTree as RootedTree>::NodeID) -> &Self::Output {
+        &self.attr[index]
+    }
+}
+
+impl IndexMut<<SimpleRootedTree as RootedTree>::NodeID> for TreeNodeAttributes{
+    fn index_mut(&mut self, index: <SimpleRootedTree as RootedTree>::NodeID) -> &mut Self::Output {
+        &mut self.attr[index]
+    }
+}
+
+impl NlcdTreeAttributes<<SimpleRootedTree as RootedTree>::NodeID, f32> for TreeNodeAttributes{}
+
 
 
 impl NearLinearCopheneticDistance for SimpleRootedTree{
     type Meta = String;
+
+    fn tree_attributes(&self, norm: u32)->impl NlcdTreeAttributes<<Self as RootedTree>::NodeID, <<Self as RootedTree>::Node as phylo::prelude::RootedZetaNode>::Zeta> {
+        TreeNodeAttributes::new(self, norm)
+    }
+
 }
